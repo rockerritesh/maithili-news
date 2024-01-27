97bb414bd1280dc339979c9e2708f2f1edba3110
Sumit Yadav <58735748+rockerritesh@users.noreply.github.com>
2024-01-27 15:16:07 +0000

    updated

--- run.py
+++ run.py
@@ -2,6 +2,19 @@ from gnews import GNews
 from datetime import datetime
 import pandas as pd
 from googletranslate import translate
+import requests
+import json
+
+def translate_text(x):
+    try:
+        translation_result = translate(x, dest='mai', src='en')
+        return translation_result
+    except requests.exceptions.RequestException as e:
+        print(f"Error making translation request: {e}")
+        return None  # Handle the error gracefully, you can modify this based on your needs
+    except json.decoder.JSONDecodeError as e:
+        print(f"Error decoding JSON response: {e}")
+        return None  # Handle the error gracefully, you can modify this based on your needs
 
 
 google_news = GNews()
@@ -18,11 +31,11 @@ for news in nepal_news:
 
 # Sort the news by date
 nepal_news.sort(key=lambda x: x["published date"])
-print("sorted news by date")
+print("--------------Sorted news by date--------------")
 
 # Assuming nepal_news is your list of dictionaries
 df = pd.json_normalize(nepal_news)
-print("normalized news")
+print("-----------Normalized news------------")
 
 # Get the full article for the last 30 news
 max_length_of_news = 20
@@ -39,23 +52,27 @@ df["images"] = (
     .apply(google_news.get_full_article)
     .apply(lambda x: x.images)
 )
-print("got full article and images")
+print("--------Got full article and images----------")
 
 # Remove non-ascii characters
 df["full_article"] = df.tail(max_length_of_news)["full_article"].apply(
     lambda x: "".join(i for i in str(x) if ord(i) < 0x10000)
 )
-print("removed non-ascii characters")
+print("--------Removed non-ascii characters----------")
 
 # Translate the full article to maithili
-df["translated"] = df.tail(max_length_of_news)["full_article"].apply(
-    lambda x: translate(x, dest="mai", src="en")
-)
-print("translated to maithili")
+# df["translated"] = df.tail(max_length_of_news)["full_article"].apply(
+#     lambda x: translate(x, dest="mai", src="en")
+# )
+
+# Assuming 'max_length_of_news' is defined
+df['translated'] = df.tail(max_length_of_news)['full_article'].apply(translate_text)
+
+print("--------Translated to maithili---------")
 
 # Save the dataframe to csv
 df.tail(max_length_of_news).to_csv("today_news.csv", index=False)
-print("saved to csv")
+print("--------Saved to csv--------")
 
 # making main datafram for store and retaive data
 main_df = pd.read_csv("filename.csv")
@@ -66,7 +83,7 @@ merged_df = (
     .drop_duplicates(subset=["title"])
     .reset_index(drop=True)
 )
-print("merged two dataframe")
+print("---------Merged two dataframe--------")
 
 # saving the main dataframe to csv
 merged_df.to_csv("filename.csv", index=False)
