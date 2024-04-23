31534d39d82689a66674fa4f23cb86e174bccfb2
Sumit Yadav <58735748+rockerritesh@users.noreply.github.com>
2024-04-23 19:16:25 +0545

    add more top news

--- run.py
+++ run.py
@@ -142,5 +142,5 @@ merged_df.to_json("docs/filename.json", orient="records")
 print("json file created successfully")
 
 # save last 100 news to json
-merged_df.tail(100).to_json("docs/last_100_news.json", orient="records")
+merged_df.tail(200).to_json("docs/last_100_news.json", orient="records")
 print("json file created successfully")
